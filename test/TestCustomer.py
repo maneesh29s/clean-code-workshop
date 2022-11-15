0843617659e86071784435cc1869cc7f9ac13a68
Shabbir Bawaji <bawaji94@gmail.com>
2022-11-15 12:11:10 +0530

    added Customer test

---
+++ test/TestCustomer.py
@@ -0,0 +1,9 @@
+import unittest
+from src.Customer import Customer
+
+class TestGetAreaRectangle(unittest.TestCase):
+    def runTest(self):
+        customer = Customer("Shabbir")
+
+if __name__ == '__main__':
+    unittest.main()
