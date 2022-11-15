6de2db42781084f4750bc78c604bc3f9e04cf448
Shabbir Bawaji <bawaji94@gmail.com>
2022-11-15 11:35:15 +0530

    Add Rental model

---
+++ src/Rental.py
@@ -0,0 +1,10 @@
+class Rental:
+    def __init__(self, movie, daysRented):
+        self.__movie = movie
+        self.__daysRented = daysRented
+
+    def getDaysRented(self):
+        self.__daysRented
+
+    def getMovie(self):
+        self.__movie
