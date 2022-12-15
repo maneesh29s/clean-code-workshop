760afea8f9c678809df07fcae5e99b30ac332d20
maneesh.sutar <maneesh.sutar@thoughtworks.com>
2022-12-15 10:15:46 +0530

    Added: html_statement

--- src/Customer.py
+++ src/Customer.py
@@ -10,16 +10,31 @@ class Customer:
         return self.__name
 
     def statement(self):
-        total_amount = 0
-        frequent_renter_points = 0
         result = "Rental Record for " + self.get_name() + "\n"
         for rental in self.__rentals:
-            rental_amount = rental.calculate_amount()
-            total_amount += rental_amount
-            frequent_renter_points += rental.calculate_frequent_renter_points()
+            result += "\t" + rental.get_movie().get_title() + "\t" + str(rental.calculate_amount()) + "\n"
 
-            result += "\t" + rental.get_movie().get_title() + "\t" + str(rental_amount) + "\n"
-
-        result += "Amount owed is " + str(total_amount) + "\n"
-        result += "You earned " + str(frequent_renter_points) + " frequent renter points"
+        result += "Amount owed is " + str(self.total_amount()) + "\n"
+        result += "You earned " + str(self.total_frequent_renter_points()) + " frequent renter points"
         return result
+
+    def html_statement(self):
+        result = "<h1>Rental Record for " + self.get_name() + "</h1><br><p>"
+        for rental in self.__rentals:
+            result += rental.get_movie().get_title() + "\t" + str(rental.calculate_amount()) + "<br>"
+
+        result += "Amount owed is " + str(self.total_amount()) + "<br>"
+        result += "You earned " + str(self.total_frequent_renter_points()) + " frequent renter points</p>"
+        return result
+
+    def total_frequent_renter_points(self):
+        frequent_renter_points = 0
+        for rental in self.__rentals:
+            frequent_renter_points += rental.calculate_frequent_renter_points()
+        return frequent_renter_points
+
+    def total_amount(self):
+        total_amount = 0
+        for rental in self.__rentals:
+            total_amount += rental.calculate_amount()
+        return total_amount
--- test/TestCustomer.py
+++ test/TestCustomer.py
@@ -9,14 +9,22 @@ class TestCustomer(unittest.TestCase):
         customer = Customer("Maneesh")
         customer.add_rental(Rental(Movie("Spiderman",Movie.NEW_RELEASE),10))
         customer.add_rental(Rental(Movie("Batman", Movie.REGULAR), 15))
-        customer.add_rental(Rental(Movie("̆Barbie", Movie.CHILDREN), 5))
+        customer.add_rental(Rental(Movie("Barbie", Movie.CHILDREN), 5))
 
         self.assertEqual(customer.statement(), """Rental Record for Maneesh
 	Spiderman	30
 	Batman	21.5
-	̆Barbie	4.5
+	Barbie	4.5
 Amount owed is 56.0
 You earned 4 frequent renter points""")
+
+    def test_should_generate_html_statement_when_asked(self):
+        customer = Customer("Maneesh")
+        customer.add_rental(Rental(Movie("Spiderman", Movie.NEW_RELEASE), 10))
+        customer.add_rental(Rental(Movie("Batman", Movie.REGULAR), 15))
+        customer.add_rental(Rental(Movie("Barbie", Movie.CHILDREN), 5))
+
+        self.assertEqual(customer.html_statement(), """<h1>Rental Record for Maneesh</h1><br><p>Spiderman	30<br>Batman	21.5<br>Barbie	4.5<br>Amount owed is 56.0<br>You earned 4 frequent renter points</p>""")
 
 
 if __name__ == '__main__':
